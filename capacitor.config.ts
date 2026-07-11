4d3bf2f865e7de65bae2ea55a847a81bad59f88b
telierlabs <zixksuper@gmail.com>
2026-07-11 11:27:19 +0000

    feat: setup native google auth and capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,12 @@ const config: CapacitorConfig = {
   server: {
     url: 'https://www.cylenai.com',
     cleartext: false
+  },
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com']
+    }
   }
 };
 
