10dcabde4f9b829b50c1b066b934f6b6498c0463
telierlabs <zixksuper@gmail.com>
2026-07-12 05:06:38 +0000

    fix: register plugin lewat terminal

--- android/app/src/main/java/com/telierlabs/cylen/MainActivity.java
+++ android/app/src/main/java/com/telierlabs/cylen/MainActivity.java
@@ -1,5 +1,13 @@
 package com.telierlabs.cylen;
 
 import com.getcapacitor.BridgeActivity;
+import com.capacitorfirebase.authentication.FirebaseAuthenticationPlugin;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    registerPlugin(FirebaseAuthenticationPlugin.class);
+  }
+}
