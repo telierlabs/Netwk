e03c41f9ef3afd28674377dda91d7b90f5dd0be6
telierlabs <zixksuper@gmail.com>
2026-07-12 07:42:00 +0000

    fix: remove invalid manual firebase auth plugin registration

--- android/app/src/main/java/com/telierlabs/cylen/MainActivity.java
+++ android/app/src/main/java/com/telierlabs/cylen/MainActivity.java
@@ -1,13 +1,5 @@
 package com.telierlabs.cylen;
 
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.community.firebaseauthentication.FirebaseAuthenticationPlugin;
-import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    registerPlugin(FirebaseAuthenticationPlugin.class);
-  }
-}
+public class MainActivity extends BridgeActivity {}
